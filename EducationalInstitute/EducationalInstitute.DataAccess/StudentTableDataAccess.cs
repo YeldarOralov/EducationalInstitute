186cc8f157e80864edc05507700c91c24e87e205
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-04-08 11:50:09 +0600

    Добавлены файлы проекта

---
+++ EducationalInstitute/EducationalInstitute.DataAccess/StudentTableDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Data.Common;
+
+namespace EducationalInstitute.DataAccess
+{
+    public class StudentTableDataAccess
+    {
+        private readonly string _connectionString;
+        private readonly DbProviderFactory _providerFactory;
+
+        public StudentTableDataAccess()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["mainAppConnectionString"].ConnectionString;
+            _providerFactory = DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["mainAppConnectionString"].ProviderName);
+        }
+        public void GetAll()
+        {
+            using (var connection = _providerFactory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                command.CommandText = "select * from Students";
+                DbDataAdapter adapter = 
+                    DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["mainAppConnectionString"].ProviderName).CreateDataAdapter();
+                adapter.SelectCommand = command;
+            }
+        }
+    }
+}
---
+++ EducationalInstitute/EducationalInstitute.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EducationalInstitute.Models")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EducationalInstitute.Models")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cb2a46ae-7f6f-4299-a342-75299c8b499d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
